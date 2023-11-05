4e782601dad74f1409a0a93bb5f2372e1eae3cad
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2023-11-05 14:47:38 +0530

    modified vite.config.ts

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
+
+cd course-app-2
+git pull origin main
+cd express-server
+pm2 kill
+pm2 start dist/index.js
--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust as needed
+  },
+});
