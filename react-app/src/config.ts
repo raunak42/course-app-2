d30874d599eafabe1ff4c8c5bbd03c06e8be78d1
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2023-11-10 20:19:11 +0530

    did npm run start locally

--- react-app/src/config.ts
+++ react-app/src/config.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://raunak42.in"; //change to http://localhost:3000 when testing locally.
+export const BASE_URL = "http://localhost:3000"; //change to http://localhost:3000 when testing locally.
 
 //hello testing, again
