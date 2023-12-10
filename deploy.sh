92c55447cb10851fe0eb968aedd13bec854c7401
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2023-12-10 11:10:04 +0530

    testing

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
 
 cd course-app-2
-sudo git pull origin main
+git pull origin main
 cd express-server
 pm2 stop index
 pm2 start dist/index.js
